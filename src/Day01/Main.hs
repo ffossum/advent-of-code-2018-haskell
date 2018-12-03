d5962fe498f472e66f5ba2a57f8540198b75ad2c
Fredrik Fossum <fredrik.fossum@finn.no>
2018-12-03 15:18:25 +0100

    Improvements to day 1

--- src/Day01/Main.hs
+++ src/Day01/Main.hs
@@ -3,8 +3,8 @@ module Day01.Main where
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as Text
 import qualified Data.Text.IO                  as TextIO
-import           Data.Set                       ( Set )
-import qualified Data.Set                      as Set
+import           Data.IntSet                    ( IntSet )
+import qualified Data.IntSet                   as IntSet
 import           Data.Attoparsec.Text
 
 main :: IO ()
@@ -16,16 +16,15 @@ main = do
   putStrLn $ "Total sum: " <> (show totalSum)
 
   let (Just firstRepeated) =
-        findFirstRepeated (scanl (+) 0 (cycle frequencies))
+        findFirstRepeated $ scanl (+) 0 (cycle frequencies)
   putStrLn $ "First sum reached twice: " <> (show firstRepeated)
 
 parseFrequency :: Text -> Either String Int
-parseFrequency = parseOnly frequency where frequency = signed decimal
+parseFrequency = parseOnly $ signed decimal
 
 findFirstRepeated :: [Int] -> Maybe Int
-findFirstRepeated as = loop Set.empty as
+findFirstRepeated = loop IntSet.empty
  where
-  loop :: Set Int -> [Int] -> Maybe Int
   loop _ [] = Nothing
-  loop seen (b : bs) =
-    if (Set.member b seen) then Just b else loop (Set.insert b seen) bs
+  loop seen (a : as) =
+    if (IntSet.member a seen) then Just a else loop (IntSet.insert a seen) as
