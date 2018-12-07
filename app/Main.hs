f7707fe61c085f2d7a2872cb043d6cc7226d6e22
Fredrik Fossum <fredrik.fossum@gmail.com>
2018-12-07 03:04:37 +0100

    solve day 5 better

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,9 @@
 module Main where
 
-import qualified Day01.Main as Day01
-import qualified Day02.Main as Day02
-import qualified Day03.Main as Day03
-
+import qualified Day01.Main                    as Day01
+import qualified Day02.Main                    as Day02
+import qualified Day03.Main                    as Day03
+import qualified Day05.Main                    as Day05
 main :: IO ()
 main = do
-  Day01.main
-  Day02.main
-  Day03.main
+  Day05.main
--- src/Day05/Main.hs
+++ src/Day05/Main.hs
@@ -1,38 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Day05.Main where
+
 import           Data.Char
-import           Data.Text                      ( Text )
-import qualified Data.Text                     as Text
-import qualified Data.Text.IO                  as TextIO
-import     qualified       Data.Attoparsec.Text as Parser
-import           Data.Attoparsec.Text
+import qualified Data.Map.Strict               as Map
 
 main :: IO ()
 main = do
-  [input] <- Text.lines <$> TextIO.readFile "src/Day05/input.txt"
-  let (Right stable) = removeReactionsUntilStable input
-  let remainingUnits = Text.length stable
+  [input] <- lines <$> readFile "src/Day05/input.txt"
+  let stable         = removeReactionsUntilStable input
+  let remainingUnits = length stable
   putStrLn $ "Remaining units after fully reacting: " ++ (show remainingUnits)
 
-removeReactionsUntilStable :: Text -> Either String Text
-removeReactionsUntilStable prev = do
-  next <- removeReactions prev
-  if (prev == next) then pure prev else removeReactionsUntilStable next
-
-removeReactions :: Text -> Either String Text
-removeReactions = parseOnly parser
-  where
-    parser :: Parser Text
-    parser = do
-      remainingText <- many' (choice [reactable, Parser.take 1])
-      pure $ Text.intercalate "" remainingText
-    reactable :: Parser Text
-    reactable = do
-      c1 <- anyChar
-      c2 <- anyChar
-      if (c1 /= c2 && (toUpper c1) == (toUpper c2))
-        then pure ""
-        else fail "no reaction"
+  let chars = ['a' .. 'z']
+  let variations = Map.mapWithKey (\k v -> filter (not . isSameType k) v)
+        $ Map.fromList (zip chars (repeat input))
+  let reduced        = removeReactionsUntilStable <$> variations
+  let shortestLength = minimum $ length <$> reduced
+  putStrLn $ "Shortest possible polymer length: " ++ (show shortestLength)
 
 
+isSameType :: Char -> Char -> Bool
+isSameType a b = a == b || toUpper a == toUpper b
+
+isReacting :: Char -> Char -> Bool
+isReacting a b = a /= b && toUpper a == toUpper b
+
+removeReactionsUntilStable :: String -> String
+removeReactionsUntilStable = foldr step ""
+ where
+  step a (b : bs) | isReacting a b = bs
+  step a bs                        = a : bs
