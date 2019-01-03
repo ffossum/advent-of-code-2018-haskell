ddab4c84c33c350a241ab7f14addcfeaf67d4433
Fredrik Fossum <fredrik.fossum@finn.no>
2019-01-03 20:35:06 +0100

    better day 14

--- src/Day14/Main.hs
+++ src/Day14/Main.hs
@@ -1,71 +1,54 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Day14.Main where
 
 import           Data.Char                      ( digitToInt
                                                 , intToDigit
                                                 )
-import           Data.Foldable                  ( toList )
+import           Data.List                      ( isPrefixOf )
 import           Data.Sequence                  ( Seq )
 import qualified Data.Sequence                 as Seq
-import           Data.Text                      ( Text )
-import qualified Data.Text                     as Text
 
 main :: IO ()
 main = do
-  let n                         = 165061
-  let (Kitchen _ finalRecipes1) = applyNTimes next testKitchen (n + 10)
+  let n = 165061
   let answer1 =
-        fmap intToDigit $ toList $ fmap recipeScore $ Seq.take 10 $ Seq.drop
-          n
-          finalRecipes1
+        fmap intToDigit $ fmap recipeScore $ (take 10 $ drop n $ allRecipes)
 
   putStrLn $ "The first ten recipes after N recipes: " ++ answer1
 
-  let needle = "165061"
-  let (Kitchen _ finalRecipes2) = applyNTimesUntil
-        next
-        testKitchen
-        100000
-        (recipesContain needle . recipes)
+  let pattern        = fmap Recipe $ fmap digitToInt $ "165061"
+  let (Just answer2) = indexOfPattern pattern allRecipes
 
-  putStrLn $ "Digits appearing before: " ++ show
-    (recipesBefore needle finalRecipes2)
+  putStrLn $ "Number of digits appearing before the pattern: " ++ show answer2
 
-recipesBefore :: Text -> Seq Recipe -> Int
-recipesBefore t rs =
-  let (b, _) = Text.breakOn t (recipesToText rs) in Text.length b
-
-recipesContain :: Text -> Seq Recipe -> Bool
-recipesContain t rs = case Text.breakOn t (recipesToText rs) of
-  (_, "") -> False
-  (_, _ ) -> True
-
-recipesToText :: Seq Recipe -> Text
-recipesToText = Text.pack . fmap intToDigit . fmap recipeScore . toList
-
+type Index = Int
 newtype Recipe = Recipe Int deriving (Eq, Show)
+
+allRecipes :: [Recipe]
+allRecipes = Recipe 3 : Recipe 7 : go [0, 1]
+                                      (Seq.fromList [Recipe 3, Recipe 7])
+ where
+  go :: [Index] -> Seq Recipe -> [Recipe]
+  go is rs =
+    let newRecipes = createNewRecipes is rs
+        rs'        = rs <> Seq.fromList newRecipes
+        is'        = fmap (nextIndex rs') is
+    in  newRecipes ++ go is' rs'
+
+indexOfPattern :: Eq a => [a] -> [a] -> Maybe Index
+indexOfPattern pattern = go 0
+ where
+  go _ [] = Nothing
+  go idx as | pattern `isPrefixOf` as = Just idx
+            | otherwise               = go (idx + 1) (tail as)
 
 recipeScore :: Recipe -> Int
 recipeScore (Recipe x) = x
 
-data Kitchen = Kitchen
-  { indices :: [Int]
-  , recipes :: Seq Recipe
-  } deriving (Eq, Show)
-
-next :: Kitchen -> Kitchen
-next k =
-  let rs' = nextRecipes k
-      is' = fmap (nextIndex rs') (indices k)
-  in  Kitchen is' rs'
-
-nextRecipes :: Kitchen -> Seq Recipe
-nextRecipes (Kitchen is rs) =
+createNewRecipes :: [Index] -> Seq Recipe -> [Recipe]
+createNewRecipes is rs =
   let currentRecipes = fmap (Seq.index rs) is
       totalScore     = sum $ fmap recipeScore $ currentRecipes
-      newRecipes     = Seq.fromList $ fmap Recipe $ digits totalScore
-  in  rs <> newRecipes
+  in  fmap Recipe $ digits totalScore
 
 nextIndex :: Seq Recipe -> Int -> Int
 nextIndex rs i =
@@ -73,17 +56,6 @@ nextIndex rs i =
       r    = rs `Seq.index` i
       i'   = (i + recipeScore r + 1) `mod` size
   in  i'
-
-applyNTimes :: (a -> a) -> a -> Int -> a
-applyNTimes f a n | n > 0     = applyNTimes f (f a) (n - 1)
-                  | otherwise = a
-
-applyNTimesUntil :: (a -> a) -> a -> Int -> (a -> Bool) -> a
-applyNTimesUntil f a n p
-  | p a       = a
-  | otherwise = applyNTimesUntil f (applyNTimes f a n) n p
-
-testKitchen = Kitchen [0, 1] (Seq.fromList [Recipe 3, Recipe 7])
 
 digits :: Int -> [Int]
 digits = map digitToInt . show . abs
